df0808506ad7248a9f660a34adf8760162508f73
Vishruti Patil <vishruti2908@gmail.com>
2023-02-26 23:18:04 +0530

    prime factor number program

--- src/com/Day5/PrimeFactors.java
+++ src/com/Day5/PrimeFactors.java
@@ -4,7 +4,7 @@ public class PrimeFactors {
     public static void main(String[] args){
         int number;
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a number ::");
+        System.out.println("Enter a number :");
         number = sc.nextInt();
 
         for(int i = 2; i< number; i++) {
